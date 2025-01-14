8377f5f21f7e6dd24a8bfbb3ca32965ec62b3770
Bruno R. Morillo <bruno@rmorillo.com>
2025-01-13 22:17:42 -0300

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import {
+  City,
+  Country,
+  LocationData,
+  State,
+} from './services/countries.interface';
 import { Countries } from './services/countries.service';
 
 export const GlobalLocations = {
   Countries,
 };
 
-export { Countries } from './services/countries.service';
+export { City, Countries, Country, LocationData, State };
